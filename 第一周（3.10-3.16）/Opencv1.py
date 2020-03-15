719870785d8e13fe37388b180c13dd01963716e9
Sustchanyuliang <44365035+Sustchanyuliang@users.noreply.github.com>
2020-03-15 14:19:30 +0800

    Update Opencv1.py

--- 第一周（3.10-3.16）/Opencv1.py
+++ 第一周（3.10-3.16）/Opencv1.py
@@ -3,8 +3,8 @@ import numpy as np
 img = cv2.imread('1.jpg')
 cv2.imshow('1', img)
 key = cv2.waitKey(0)
-if key == 27:
+if key == 27: # 'ESC'
     cv2.destroyAllWindows()
-elif key == 115:
+elif key == 115: # 's'
     cv2.imwrite('a.bmp',img)
     cv2.destroyAllWindows()

f9afb09b1db61d3eefce554de5002abad2aec21d
Sustchanyuliang <44365035+Sustchanyuliang@users.noreply.github.com>
2020-03-15 11:59:41 +0800

    Add files via upload

--- 第一周（3.10-3.16）/Opencv1.py
+++ 第一周（3.10-3.16）/Opencv1.py
@@ -1,6 +1,10 @@
 import cv2
 import numpy as np
-
-img = cv2.imread("1.jpg")
-cv2.imshow("1", img)
-cv2.waitKey(10000)
+img = cv2.imread('1.jpg')
+cv2.imshow('1', img)
+key = cv2.waitKey(0)
+if key == 27:
+    cv2.destroyAllWindows()
+elif key == 115:
+    cv2.imwrite('a.bmp',img)
+    cv2.destroyAllWindows()
